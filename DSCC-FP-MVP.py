5820852e1ac0dee273762be1d1aed1f11e7b10fe
Anaekh Dheepak <anaekh@gmail.com>
2022-09-22 10:53:38 -0400

    download_data

--- DSCC-FP-MVP.py
+++ DSCC-FP-MVP.py
@@ -1,2 +1,9 @@
 import yfinance as yf
 import pandas as pd
+
+mst = yf.Ticker('SMSN.IL')
+print(mst.info)
+print(type(mst))
+
+data = yf.download('SMSN.IL', start='2021-01-01', end='2021-12-31')
+data.to_csv('SMSN.csv')
