e4d0e18de9f0cf6197636188c56dd968979f95b5
Anaekh Dheepak <anaekh@gmail.com>
2022-10-04 15:56:35 -0400

    InitialFile

--- DSCC-FP-MVP.py
+++ DSCC-FP-MVP.py
@@ -1,9 +1,22 @@
 import yfinance as yf
 import pandas as pd
 
-mst = yf.Ticker('SMSN.IL')
-print(mst.info)
-print(type(mst))
 
-data = yf.download('SMSN.IL', start='2021-01-01', end='2021-12-31')
-data.to_csv('SMSN.csv')
+class FetchData:
+    def __init__(self, ticker) -> None:
+        self.ticker = ticker
+
+    def fetch_data_from_to_date(self, start_date='2021-01-01', end_date='2021-12-31'):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.fetch_data = yf.download(
+            self.ticker, start=self.start_date, end=self.end_date)
+        return self.fetch_data
+
+    def write_to_csv(self, filename):
+        return self.fetch_data_from_to_date(self.start_date, self.end_date).to_csv(filename)
+
+
+data_aapl = FetchData('AAPL')
+print(data_aapl.fetch_data_from_to_date())
+data_aapl.write_to_csv('test1.csv')
