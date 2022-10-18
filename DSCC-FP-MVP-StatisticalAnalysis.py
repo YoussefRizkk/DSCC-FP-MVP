3d8d5aea29e8c52bb2e3ce142015b64f6c565e66
Anaekh Dheepak <anaekh@gmail.com>
2022-10-18 16:13:13 -0400

    UsedSQLAlchemy&mysqlconnector

--- DSCC-FP-MVP-StatisticalAnalysis.py
+++ DSCC-FP-MVP-StatisticalAnalysis.py
@@ -19,26 +19,9 @@ connect_db = mysql.connector.connect(
     password='password'
 )
 
-print(f'Conncted to database {connect_db}')
-# Create a cursor object
-db_cursor_object = connect_db.cursor()
-
-# Fetch data and put in a dataframe
-db_cursor_object.execute('USE stock_database')
-db_cursor_object.execute('SELECT * FROM AAPL')
-# print(db_cursor_object)
-asd = db_cursor_object.fetchall()
-# print(asd)
-df = pd.DataFrame(asd, columns=my_dict.keys())
-# print(df)
-
-df1 = pd.read_sql_query('SELECT * FROM AAPL', connect_db)
-print(df1)
-# Statistical Analysis
-
 # Create an SQL engine
 connect_engine = create_engine(
-    "mysql+pymysql://root:password@localhost/stock_database"
+    "mysql+mysqlconnector://root:password@localhost/stock_database"
 )
 
 df1 = pd.read_sql_query('SELECT * FROM AAPL', connect_engine)
